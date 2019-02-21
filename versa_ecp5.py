70fb59b352edb8d298affa2763c4450e07df227e
Florent Kermarrec <florent@enjoy-digital.fr>
2019-02-21 15:50:33 +0100

    versa_ecp5: create a DevSoC (to prototype the PHY) and a BaseSoC (to test the full stack)

--- versa_ecp5.py
+++ versa_ecp5.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 import argparse
 
 from migen import *
@@ -17,9 +18,9 @@ from litescope import LiteScopeAnalyzer
 
 from liteeth.common import *
 from liteeth.core import LiteEthUDPIPCore
-from liteeth.frontend.etherbone import LiteEthEtherbone
 
 from ecp5rgmii import LiteEthPHYRGMII
+
 
 class _CRG(Module):
     def __init__(self, platform, sys_clk_freq):
@@ -40,20 +41,55 @@ class _CRG(Module):
         self.specials += AsyncResetSynchronizer(self.cd_sys, ~pll.locked | ~rst_n)
 
 
-class BaseSoC(SoCCore):
+class DevSoC(SoCCore):
     csr_map = {
-        "ddrphy":    16,
         "analyzer":  17
     }
     csr_map.update(SoCCore.csr_map)
-    def __init__(self, **kwargs):
+    def __init__(self):
+        platform = versa_ecp5.Platform(toolchain="diamond")
+        sys_clk_freq = int(133e6)
+        SoCCore.__init__(self, platform, clk_freq=sys_clk_freq,
+                         cpu_type=None, with_uart=False,
+                         csr_data_width=32,
+                         ident="Versa ECP5 test SoC", ident_version=True)
+
+        # crg
+        self.submodules.crg = _CRG(platform, sys_clk_freq)
+
+        # uart
+        self.submodules.bridge = UARTWishboneBridge(platform.request("serial"), sys_clk_freq, baudrate=115200)
+        self.add_wb_master(self.bridge.wishbone)
+
+        # ethernet phy
+        ethphy = LiteEthPHYRGMII(platform.request("eth_clocks"),
+                        platform.request("eth"))
+        self.submodules += ethphy
+
+        # led blinking
+        led_counter = Signal(32)
+        self.sync += led_counter.eq(led_counter + 1)
+        self.comb += platform.request("user_led", 0).eq(led_counter[26])
+
+        # analyzer
+        analyzer_signals = [
+            ethphy.source
+        ]
+        self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals, 1024, "eth_rx")
+
+    def do_exit(self, vns):
+        if hasattr(self, "analyzer"):
+            self.analyzer.export_csv(vns, "test/analyzer.csv")
+
+
+class BaseSoC(SoCCore):
+    def __init__(self):
         platform = versa_ecp5.Platform(toolchain="diamond")
         sys_clk_freq = int(133e6)
         SoCCore.__init__(self, platform, clk_freq=sys_clk_freq,
                           cpu_type=None, with_uart=False,
                           csr_data_width=32,
-                          ident="Versa ECP5 test SoC", ident_version=True,
-                          **kwargs)
+                          ident="Versa ECP5 test SoC", ident_version=True)
 
         # crg
         self.submodules.crg = _CRG(platform, sys_clk_freq)
@@ -68,36 +104,19 @@ class BaseSoC(SoCCore):
                                    with_icmp=True)
         self.submodules += ethphy, ethcore
 
-        # etherbone bridge
-        self.submodules.etherbone = LiteEthEtherbone(ethcore.udp, 1234)
-        self.add_wb_master(self.etherbone.wishbone.bus)
-
         ethphy.crg.cd_eth_rx.clk.attr.add("keep")
         ethphy.crg.cd_eth_tx.clk.attr.add("keep")
         platform.add_period_constraint(ethphy.crg.cd_eth_rx.clk, period_ns(125e6))
         platform.add_period_constraint(ethphy.crg.cd_eth_tx.clk, period_ns(125e6))
-
-        # uart
-        self.submodules.bridge = UARTWishboneBridge(platform.request("serial"), sys_clk_freq, baudrate=115200)
-        self.add_wb_master(self.bridge.wishbone)
 
         # led blinking
         led_counter = Signal(32)
         self.sync += led_counter.eq(led_counter + 1)
         self.comb += platform.request("user_led", 0).eq(led_counter[26])
 
-        # analyzer
-        analyzer_signals = [
-            Signal(16)
-        ]
-        self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals, 128)
-
-    def do_exit(self, vns):
-        if hasattr(self, "analyzer"):
-            self.analyzer.export_csv(vns, "test/analyzer.csv")
 
 def main():
-    soc = BaseSoC()
+    soc = DevSoC() if "dev" in sys.argv[1:] else BaseSoC()
     builder = Builder(soc, output_dir="build", csr_csv="test/csr.csv")
     vns = builder.build(toolchain_path="/usr/local/diamond/3.10_x64/bin/lin64")
     soc.do_exit(vns)
